011b7a2d6e46d35e688a6fa39961146a1fb8c960
开盛 <mengpeiwei@cai-inc.com>
2026-01-07 15:52:29 +0800

    feat: 接口 HOST

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
-export const HOST =  'http://ai.xiaoshengkai.com'
+// export const HOST =  'http://ai.xiaoshengkai.com'
+export const HOST =  `${location.origin}/:3000`
 // export const HOST =  'http://localhost:4111'
