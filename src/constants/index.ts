231f1bfc7152cfcd917e9691595d3902baef87ff
开盛 <mengpeiwei@cai-inc.com>
2026-01-12 16:03:47 +0800

    feat: 思考模式

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 // export const HOST =  'http://ai.xiaoshengkai.com'
-// export const HOST =  `${location.origin}:3000`
-export const HOST =  'http://localhost:4111'
+export const HOST =  `${location.origin}:3000`
+// export const HOST =  'http://localhost:4111'
